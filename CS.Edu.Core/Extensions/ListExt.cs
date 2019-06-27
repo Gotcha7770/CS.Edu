5ddf59abcd59f7d60f7a54c23fcbfbb39e741476
gleb.bakanov <gleb.bakanov@geosteertech.com>
2019-06-27 11:31:11 +0300

    +

--- CS.Edu.Core/Extensions/ListExt.cs
+++ CS.Edu.Core/Extensions/ListExt.cs
@@ -10,7 +10,7 @@ namespace CS.Edu.Core.Extensions
         {
             for (int i = 0; i < source.Count; i += count)
             {
-                yield return source.GetRange(i, count);
+                yield return source.GetRange(i, Math.Min(count, source.Count - i));
             }
         }
     }
