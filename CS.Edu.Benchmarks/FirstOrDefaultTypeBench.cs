08d15275eb5e9d4496ccefad58ef063bbb8158bf
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-08-12 11:24:16 +0300

    select of type bench

---
+++ CS.Edu.Benchmarks/FirstOrDefaultTypeBench.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using CS.Edu.Core.Extensions;
+
+namespace CS.Edu.Benchmarks
+{
+    public enum ClassType
+    {
+        Type1,
+        Type2
+    }
+
+    public interface ITyped
+    {
+        ClassType Type { get; }
+
+        int Value { get; set; }
+    }
+
+    public class Type1 : ITyped
+    {
+        public ClassType Type => ClassType.Type1;
+
+        public int Value { get; set; }
+    }
+
+    public class Type2 : ITyped
+    {
+        public ClassType Type => ClassType.Type2;
+
+        public int Value { get; set; }
+
+    }
+
+    [Config(typeof(DefaultConfig))]
+    public class FirstOrDefaultTypeBench
+    {
+        private readonly ITyped[] Items = Enumerable.Range(0, 1000)
+            .Select(x => x.IsEven() ? (ITyped)new Type1 { Value = x } : new Type2 { Value = x })
+            .ToArray();
+
+        [Benchmark]
+        public object GetFirstOrDeafaultOfType()
+        {
+            return Items.OfType<Type1>().FirstOrDefault(x => x.Value == 500);
+        }
+
+        [Benchmark]
+        public object GetFirstOrDeafaultWhere()
+        {
+            return Items.FirstOrDefault(x => x.Type == ClassType.Type1 && x.Value == 500) as Type1;
+        }
+    }
+}
