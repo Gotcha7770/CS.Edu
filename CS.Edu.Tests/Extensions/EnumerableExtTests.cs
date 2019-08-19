65ceed893376b14cf4fca70f79305e007e5b9083
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-08-19 09:41:53 +0300

    Cleanup

--- CS.Edu.Tests/Extensions/EnumerableExtTests.cs
+++ CS.Edu.Tests/Extensions/EnumerableExtTests.cs
@@ -20,7 +20,7 @@ namespace CS.Edu.Tests.Extensions
         [Test]
         public void NullOrEmpty_Empty_ReturnsTrue()
         {
-            var empty = Array.Empty<int>();
+            var empty = Enumerable.Empty<int>();
             Assert.That(empty.IsNullOrEmpty(), Is.True);
         }
 
@@ -34,37 +34,45 @@ namespace CS.Edu.Tests.Extensions
         [Test]
         public void TakeWhile_WhilePrevLessThenNext_ReturnsFirst3Elements()
         {
-            var items = new List<int> { 1, 2, 3, 2, 1 };
+            var items = new int[] { 1, 2, 3, 2, 1 };
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.TakeWhile(lessThan).ToArray(), Is.EqualTo(new int[] { 1, 2, 3 }));
+            var result = items.TakeWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(new int[] { 1, 2, 3 }));
         }
 
         [Test]
         public void TakeWhile_OneElement_ReturnsThatElement()
         {
-            var items = new List<int> { 1 };
+            var items = new int[] { 1 };
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.TakeWhile(lessThan).ToArray(), Is.EqualTo(new int[] { 1 }));
+            var result = items.TakeWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(new int[] { 1 }));
         }
 
         [Test]
         public void TakeWhile_Empty_ReturnsEmpty()
         {
-            var items = Array.Empty<int>();
+            var items = Enumerable.Empty<int>();
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.TakeWhile(lessThan).ToArray(), Is.EqualTo(new int[0]));
+            var result = items.TakeWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(Array.Empty<int>()));
         }
 
         [Test]
         public void SkipWhile_WhilePrevLessThenNext_SkipFirst2Elements()
         {
-            var items = new List<int> { 1, 2, 3, 2, 1 };
+            var items = new int[] { 1, 2, 3, 2, 1 };
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.SkipWhile(lessThan).ToArray(), Is.EqualTo(new int[] { 2, 1 }));
+            var result = items.SkipWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(new int[] { 2, 1 }));
         }
 
         [Test]
@@ -73,7 +81,9 @@ namespace CS.Edu.Tests.Extensions
             var items = new List<int> { 1 };
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.SkipWhile(lessThan).ToArray(), Is.EqualTo(new int[0]));
+            var result = items.SkipWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(Array.Empty<int>()));
         }
 
         [Test]
@@ -82,32 +92,41 @@ namespace CS.Edu.Tests.Extensions
             var items = Array.Empty<int>();
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.SkipWhile(lessThan).ToArray(), Is.EqualTo(new int[0]));
+            var result = items.SkipWhile(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(Array.Empty<int>()));
         }
 
         [Test]
         public void Split_Empty_ReturnsEmpty()
         {
-            var items = Array.Empty<int>();
+            var items = Enumerable.Empty<int>();
             Relation<int> lessThan = (x, y) => x < y;
 
-            Assert.That(items.Split(lessThan).ToArray(), Is.EqualTo(new int[0]));
+            var result = items.Split(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(Array.Empty<int>()));
         }
 
         [Test]
-        public void Split_OneElement_ReturnsEmpty() //???
+        public void Split_OneElement_ReturnsThatElement()
         {
-            var items = new List<int> { 1 };
+            var items = new int[] { 1 };
             Relation<int> lessThan = (x, y) => x < y;
+
+            var result = items.Split(lessThan).ToArray();
+
+            Assert.That(result, Is.EqualTo(new int[] { 1 }));
         }
 
         [Test]
         public void Split_WhilePrevLessThenNext_Returns2Groups()
         {
-            var items = new List<int> { 1, 2, 3, 2, 3 };
+            var items = new int[] { 1, 2, 3, 2, 3 };
             Relation<int> lessThan = (x, y) => x < y;
 
             var result = items.Split(lessThan).ToArray();
+
             Assert.That(result.Length, Is.EqualTo(2));
             Assert.That(result[0], Is.EqualTo(new int[] { 1, 2, 3 }));
             Assert.That(result[1], Is.EqualTo(new int[] { 2, 3 }));
@@ -131,7 +150,9 @@ namespace CS.Edu.Tests.Extensions
         {
             int pageSize = 20;
             var items = Enumerable.Range(0, 85);
+
             var paginated = items.Paginate(pageSize).ToArray();
+
             Assert.That(paginated, Is.Not.Empty);
             Assert.That(paginated.Length, Is.EqualTo(((items.Count() - 1) / pageSize) + 1));
             Assert.That(paginated.First(), Is.InstanceOf<IEnumerable>());
@@ -141,8 +162,10 @@ namespace CS.Edu.Tests.Extensions
         [Test]
         public void Paginate_EmptyEnumerable_ReturnsEmptyEnumerable()
         {
-            var items = Array.Empty<int>();
+            var items = Enumerable.Empty<int>();
+
             var paginated = items.Paginate(5);
+
             Assert.That(paginated, Is.Not.Null);
             Assert.That(paginated, Is.Empty);
         }
