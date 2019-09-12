25cad0a42a674fc0300fe007a85e9dda7cb23888
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-09-12 16:39:45 +0300

    +

--- CS.Edu.Benchmarks/FirstOrDefaultTypeBench.cs
+++ CS.Edu.Benchmarks/FirstOrDefaultTypeBench.cs
@@ -1,39 +1,33 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using BenchmarkDotNet.Attributes;
-using CS.Edu.Core.Extensions;
 
 namespace CS.Edu.Benchmarks
 {
-    public class NumberType
-    {
-        public int Number { get; set; }
-    }
-
-    public class StringType
-    {
-        public string Value { get; set; }
-    }
+    public class SomeType { }
 
     [Config(typeof(DefaultConfig))]
     public class FirstOrDefaultTypeBench
     {
         private readonly object[] Items = Enumerable.Range(0, 1000)
-            .Select(x => x == 500 ? (object)new NumberType { Number = x } : new StringType { Value = x.ToString() })
+            .Select(x => new object())
             .ToArray();
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            Items[499] = new SomeType();
+        }
 
         [Benchmark]
         public object GetFirstOrDefaultOfType()
         {
-            return Items.OfType<NumberType>().FirstOrDefault();
+            return Items.OfType<SomeType>().FirstOrDefault();
         }
 
         [Benchmark]
         public object GetFirstOrDefaultWhereTypeCheck()
         {
-            return Items.FirstOrDefault(x => x is NumberType) as NumberType;
+            return Items.FirstOrDefault(x => x is SomeType) as SomeType;
         }
     }
 }
