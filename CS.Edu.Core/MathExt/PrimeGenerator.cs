c915bd693e0b03d96ccd9344a7a5d360ffdcfac7
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-12-02 10:33:02 +0300

    primes generator naive implementation

--- CS.Edu.Core/MathExt/PrimeGenerator.cs
+++ CS.Edu.Core/MathExt/PrimeGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using CS.Edu.Core.Extensions;
 
 namespace CS.Edu.Core.MathExt
 {
@@ -27,8 +28,10 @@ namespace CS.Edu.Core.MathExt
 
     class PrimesEnumerator : IEnumerator<long>
     {
-        private long _current = 2;
-        private HashSet<long> _factors = new HashSet<long>();
+        private long _current = 0;
+        private int _step = 0;
+        
+        private List<long> _factors = new List<long>{2, 3};
 
         public long Current => _current;
 
@@ -41,13 +44,60 @@ namespace CS.Edu.Core.MathExt
 
         public bool MoveNext()
         {
-            throw new NotImplementedException();
+            if (_current == 0)
+            {
+                _current = 2;
+                return true;
+            }
+
+            if(_current == 2)
+            {
+                _current = 3;
+                return true;
+            }                
+
+            _current += GetNextStep();
+
+            while(_current < long.MaxValue )
+            {                
+                if(!HasDeviders(_current))
+                {
+                    _factors.Add(_current);
+                    return true;
+                }
+
+                _current += GetNextStep();                       
+            }
+
+            return false;
+        }
+
+        private bool HasDeviders(long current)
+        {
+            foreach (long factor in _factors)
+            {
+                if(current % factor == 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private int GetNextStep()
+        {
+            return (_step++) switch
+            {                
+                0 => 2,
+                int x when x.IsEven()=> 4,
+                _ => 2
+            };
         }
 
         public void Reset()
         {
-            _current = 2;
-            _factors.Clear();
+            _current = 0;
+            _step = 0;
+            _factors = new List<long> { 2, 3};
         }
     }
 }
