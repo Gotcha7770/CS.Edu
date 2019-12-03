d19e28f8b2c3d4406e7d2c63af932fa7a0b59436
Gleb Bakanov <gbakanov@inbox.ru>
2019-12-03 23:29:08 +0300

    variants

--- CS.Edu.Core/Extensions/FuncExt.cs
+++ CS.Edu.Core/Extensions/FuncExt.cs
@@ -4,17 +4,17 @@ namespace CS.Edu.Core.Extensions
 {
     public static class FuncExt
     {
-        public static Func<A, C> Compose<A, B, C>(Func<A, B> f, Func<B, C> g)
+        public static Func<A, C> Compose<A, B, C>(this Func<A, B> f, Func<B, C> g)
         {
             return (x) => g(f(x));
         }
 
-        public static Func<T2, TResult> ApplyPartial<T1, T2, TResult>(Func<T1, T2, TResult> function, T1 arg)
+        public static Func<T2, TResult> ApplyPartial<T1, T2, TResult>(this Func<T1, T2, TResult> function, T1 arg)
         {
             return (x) => function(arg, x);
         }
 
-        public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> function)
+        public static Func<T1, Func<T2, Func<T3, TResult>>> Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> function)
         {
             return a => b => c => function(a, b, c);
         }
--- CS.Edu.Core/MathExt/PrimeGenerator.cs
+++ CS.Edu.Core/MathExt/PrimeGenerator.cs
@@ -5,12 +5,44 @@ using System.Linq;
 using CS.Edu.Core.Extensions;
 
 namespace CS.Edu.Core.MathExt
-{
+{    
     public static class PrimesGenerator
     {
+        public static IEnumerable<R> Magic<T, R>(this IEnumerable<T> source,
+                                                 Predicate<T> condition,
+                                                 Func<T, R> transform,
+                                                 Action<T> sideEffect)
+        {
+            return source.Select(x => condition(x) ? x : transform(x))
+        }
+
         public static IEnumerable<long> GetPrimes()
         {
             return PrimesIterator();
+        }
+
+        private static IEnumerable<long> PrimesIterator2()
+        {
+            //var factors = new List<long> { 2 };
+            
+            Func<List<long>, long, long> func = (factors, num) => 
+            {
+                if(factors.All(x => num % x != 0)) //condition
+                {
+                    factors.Add(num); //side effect
+                    return num; //result
+                }
+
+                return 0; //result
+            };
+
+            //var tmp = func.ApplyPartial(new List<long> { 2 });
+
+            return EnumerableEx.Generate(3L, x => x < long.MaxValue, x => ++x, x => x)
+                .Where(x => !x.IsEven())
+                .Select(x => func.ApplyPartial(new List<long> { 2 })(x))
+                .Where(x => x != 0)
+                .Prepend(2);
         }
 
         private static IEnumerable<long> PrimesIterator()
