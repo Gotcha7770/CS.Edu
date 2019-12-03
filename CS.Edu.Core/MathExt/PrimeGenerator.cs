33ec71efb501ef8ecafbca3dc92e0d7739a21d38
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-12-03 10:11:26 +0300

    primes generator refactoring

--- CS.Edu.Core/MathExt/PrimeGenerator.cs
+++ CS.Edu.Core/MathExt/PrimeGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using CS.Edu.Core.Extensions;
 
 namespace CS.Edu.Core.MathExt
@@ -8,96 +9,30 @@ namespace CS.Edu.Core.MathExt
     public static class PrimesGenerator
     {
         public static IEnumerable<long> GetPrimes()
-        {            
-            return new PrimesIterator();
-        }
-    }
-
-    class PrimesIterator : IEnumerable<long>
-    {
-        public IEnumerator<long> GetEnumerator()
         {
-            return new PrimesEnumerator();
+            return PrimesIterator();
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        private static IEnumerable<long> PrimesIterator()
         {
-            return new PrimesEnumerator();
-        }
-    }
+            yield return 2;
+            yield return 3;
 
-    class PrimesEnumerator : IEnumerator<long>
-    {
-        private long _current = 0;
-        private int _step = 0;
-        
-        private List<long> _factors = new List<long>{2, 3};
+            long current = 5;
+            int step = 0;
+            var factors = new List<long> { 2, 3 };
 
-        public long Current => _current;
-
-        object IEnumerator.Current => _current;
-
-        public void Dispose()
-        {
-            
-        }
-
-        public bool MoveNext()
-        {
-            if (_current == 0)
+            while (current < long.MaxValue)
             {
-                _current = 2;
-                return true;
-            }
-
-            if(_current == 2)
-            {
-                _current = 3;
-                return true;
-            }                
-
-            _current += GetNextStep();
-
-            while(_current < long.MaxValue )
-            {                
-                if(!HasDeviders(_current))
+                if (factors.All(x => current % x != 0))
                 {
-                    _factors.Add(_current);
-                    return true;
+                    factors.Add((int)current);
+                    yield return current;
                 }
 
-                _current += GetNextStep();                       
+                current += step.IsEven() ? 2 : 4;
+                ++step;
             }
-
-            return false;
         }
-
-        private bool HasDeviders(long current)
-        {
-            foreach (long factor in _factors)
-            {
-                if(current % factor == 0)
-                    return true;
-            }
-
-            return false;
-        }
-
-        private int GetNextStep()
-        {
-            return (_step++) switch
-            {                
-                0 => 2,
-                int x when x.IsEven()=> 4,
-                _ => 2
-            };
-        }
-
-        public void Reset()
-        {
-            _current = 0;
-            _step = 0;
-            _factors = new List<long> { 2, 3};
-        }
-    }
+    }    
 }
