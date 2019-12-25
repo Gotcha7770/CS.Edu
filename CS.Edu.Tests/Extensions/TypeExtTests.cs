0fd05b0478871c08ff71c755ebaa88ee69ecf523
Gleb Bakanov <gleb.bakanov@geosteertech.com>
2019-12-25 14:07:21 +0300

    +

--- CS.Edu.Tests/Extensions/TypeExtTests.cs
+++ CS.Edu.Tests/Extensions/TypeExtTests.cs
@@ -4,13 +4,17 @@ using NUnit.Framework;
 
 namespace CS.Edu.Tests
 {
-    class TestTypeBase { }
+    class TBase { }
+    
+    class FirstLevelType : TBase { }
 
-    class TestType : TestTypeBase { }
+    class TestType : FirstLevelType { }
 
-    class TestGenericBase<T> where T : TestTypeBase { }
+    class BaseGeneric<T> where T : TBase { }
 
-    class TestClass : TestGenericBase<TestType> { }
+    class FirstLevelGeneric<T> : BaseGeneric<FirstLevelType> { }
+
+    class TestClass : FirstLevelGeneric<TestType> { }
 
     [TestFixture]
     public class TypeExtTests
@@ -19,15 +23,22 @@ namespace CS.Edu.Tests
         [Test]
         public void TestMethod()
         {
-            Type openBaseType = typeof(TestGenericBase<>);
-            Type baseType = typeof(TestGenericBase<TestTypeBase>);
-            Type testTypeGeneric = typeof(TestGenericBase<TestType>);
+            Type openBaseType = typeof(BaseGeneric<>);
+            Type baseType = typeof(BaseGeneric<TBase>);
+            Type testTypeGeneric = typeof(BaseGeneric<TestType>);
             Type testType = typeof(TestClass);
 
             //object obj = new TestClass();
 
             Assert.IsTrue(testType.IsSubclassOf(testTypeGeneric));
             Assert.IsTrue(testType.IsSubclassOfExt(openBaseType));
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            var cast1 = new TestClass() as BaseGeneric<TestType>;
+            var cast2 = new TestClass() as BaseGeneric<TBase>;
         }
     }
 }
