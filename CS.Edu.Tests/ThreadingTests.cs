7be079f8dbe204b6891081ccc0a572ec7ce08e01
Gotcha7770 <gbakanov@inbox.ru>
2020-01-22 11:17:35 +0300

    threading tests

---
+++ CS.Edu.Tests/ThreadingTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CS.Edu.Core;
+using CS.Edu.Core.Extensions;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests
+{
+    [TestFixture]
+    public class ThreadingTests
+    {
+        private static int _doggosPetted;        
+
+        [Test]
+        public void IncremetFromDifferentThreads()
+        {
+            void PetDoggo()
+            {
+                _doggosPetted += 1;
+            }
+
+            var tasks = Enumerable.Range(0, 50)
+                .Select(x => Task.Run(PetDoggo))
+                .ToArray();
+
+            Parallel.For(1, 50, (i, s) => 
+            {
+                PetDoggo();
+            });
+
+            Assert.That(_doggosPetted, Is.Not.EqualTo(50));
+        }
+    }
+}
