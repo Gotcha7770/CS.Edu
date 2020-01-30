02d947dea09ed0002d62e1187fec52d6e0d0db76
Gleb Bakanov <gbakanov@inbox.ru>
2020-01-30 22:51:54 +0300

    delegate converters

--- CS.Edu.Core/Extensions/DelegateExtensions.cs
+++ CS.Edu.Core/Extensions/DelegateExtensions.cs
@@ -4,6 +4,20 @@ namespace CS.Edu.Core.Extensions
 {
     public static class DelegateExtensions
     {
+        public static Func<T, T, bool> ToFunc<T>(this Relation<T> relation)
+        {
+            return new Func<T, T, bool>((x, y) => relation(x,y));
+        }
+
+        public static Func<T1, T2, bool> ToFunc<T1, T2>(this Relation<T1, T2> relation)
+        {
+            return new Func<T1, T2, bool>((x, y) => relation(x,y));
+        }
+
+        public static Func<T, T, T> ToFunc<T>(this Merge<T> mergeFunc)
+        {
+            return new Func<T, T, T>((x,y) => mergeFunc(x, y));
+        } 
 
         public static Predicate<T> True<T>() => TruePredicate<T>.Value;
 
@@ -30,6 +44,6 @@ namespace CS.Edu.Core.Extensions
             internal static readonly Func<T, T> Value = Id;
 
             private static T Id(T value) => value;
-        }
+        }        
     }
 }
