13d8eb1167719d243fddd816f20e81a6dd7ee341
Gleb Bakanov <gbakanov@inbox.ru>
2020-04-09 13:08:29 +0300

    add tail method

--- CS.Edu.Core/Extensions/ObservableExt.cs
+++ CS.Edu.Core/Extensions/ObservableExt.cs
@@ -2,7 +2,9 @@ using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using DynamicData;
 
 namespace CS.Edu.Core.Extensions
 {
@@ -25,6 +27,48 @@ namespace CS.Edu.Core.Extensions
                 x => source.PropertyChanged -= x)
             .Where(x => propertyName.Equals(x.EventArgs.PropertyName, StringComparison.InvariantCulture))
             .Select(x => getter(source));
+        }
+
+        public static IObservable<IChangeSet<T>> Tail<T>(this IObservable<IChangeSet<T>> source,
+                                                         int numberOfItems)
+        {
+            return Observable.Create<IChangeSet<T>>(observer =>
+            {
+                var request = new TailRequest<T>(source, numberOfItems);
+
+                return new CompositeDisposable
+                {
+                    request,
+                    source.Virtualise(Observable.Return(request)).SubscribeSafe(observer)
+                };
+            });
+        }
+
+        public class TailRequest<T> : IVirtualRequest, IDisposable
+        {
+            private readonly IDisposable _subscription;
+            private int _count;            
+
+            public TailRequest(IObservable<IChangeSet<T>> source, int numberOfItems)
+            {
+                _subscription = source.Subscribe(RefreshStartIndex);
+                Size = numberOfItems;
+            }
+
+            public int Size { get; }
+
+            public int StartIndex => _count > Size ? _count - Size : 0;
+
+            private void RefreshStartIndex(IChangeSet<T> changeSet)
+            {
+                _count += changeSet.Adds;
+                _count -= changeSet.Removes;
+            }
+
+            public void Dispose()
+            {
+                _subscription.Dispose();
+            }
         }
     }
 }
