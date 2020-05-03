5b8da28efc519074d0ec48aaf8bcf8e5e4dc04a3
Gleb Bakanov <gbakanov@inbox.ru>
2020-05-03 07:32:30 +0300

    predicate tests

--- CS.Edu.Core/Extensions/Predicates.cs
+++ CS.Edu.Core/Extensions/Predicates.cs
@@ -8,6 +8,16 @@ namespace CS.Edu.Core.Extensions
 
         public static Predicate<T> False<T>() => FalsePredicate<T>.Value;
 
+        public static Predicate<T> And<T>(this Predicate<T> one, Predicate<T> other)
+        {
+            return x => one(x) && other(x);
+        }
+
+        public static Predicate<T> Or<T>(this Predicate<T> one, Predicate<T> other)
+        {
+            return x => one(x) || other(x);
+        }
+
         private static class TruePredicate<T>
         {
             internal static readonly Predicate<T> Value = AlwaysTrue;
---
+++ CS.Edu.Tests/Extensions/PredicatesTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CS.Edu.Core.Extensions;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests.Extensions
+{
+    [TestFixture]
+    public class PredicatesTests
+    {
+        [TestCase(2, ExpectedResult = false)]
+        [TestCase(3, ExpectedResult = false)]
+        [TestCase(6, ExpectedResult = true)]
+        [TestCase(10, ExpectedResult = false)]
+        [TestCase(12, ExpectedResult = true)]
+        public bool PredicatesAndTest(int value)
+        {
+            Predicate<int> predicate1 = x => x % 2 == 0;
+            Predicate<int> predicate2 = x => x % 3 == 0;
+            Predicate<int> predicate3 = predicate1
+                .And(predicate2);
+
+            return predicate3(value);
+        }
+
+        [TestCase(2, ExpectedResult = true)]
+        [TestCase(3, ExpectedResult = true)]
+        [TestCase(4, ExpectedResult = true)]
+        [TestCase(5, ExpectedResult = false)]
+        [TestCase(6, ExpectedResult = true)]
+        public bool PredicatesOrTest(int value)
+        {
+            Predicate<int> predicate1 = x => x % 2 == 0;
+            Predicate<int> predicate2 = x => x % 3 == 0;
+            Predicate<int> predicate3 = predicate1
+                .Or(predicate2);
+
+            return predicate3(value);
+        }
+    }
+}
