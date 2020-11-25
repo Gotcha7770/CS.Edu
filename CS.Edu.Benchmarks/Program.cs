623e100a4fc54d0f6d850a09734b6f0f308a33a4
Gleb Bakanov <gbakanov@geonaft.ai>
2020-11-25 20:09:05 +0300

    remove redundant benchmark

--- CS.Edu.Benchmarks/Program.cs
+++ CS.Edu.Benchmarks/Program.cs
@@ -29,7 +29,6 @@ namespace CS.Edu.Benchmarks
                 typeof(JaggedVsRectArrayBench),
                 typeof(DictionaryVsLookupBench),
                 typeof(DoubleEnumerationBench),
-                typeof(TestBench),
                 typeof(PullOutBench),
                 typeof(ExceptBench),
                 typeof(JoinBench),
