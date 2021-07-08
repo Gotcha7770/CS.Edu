3730184c6aa455e61eae66fc60a5747767e8a455
Gotcha7770 <gbakanov@inbox.ru>
2021-07-08 14:42:24 +0300

    memoization

--- CS.Edu.Core/Extensions/Functions.cs
+++ CS.Edu.Core/Extensions/Functions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 
 namespace CS.Edu.Core.Extensions
 {
@@ -42,6 +43,21 @@ namespace CS.Edu.Core.Extensions
         public static Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> Curry<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> function)
         {
             return a => b => c => d => function(a, b, c, d);
+        }
+
+        /// <summary>
+        /// <see cref="https://stackoverflow.com/questions/1254995/thread-safe-memoization"/>
+        /// </summary>
+        public static Func<TIn, TOut> Memoize<TIn, TOut>(this Func<TIn, TOut> function)
+        {
+            var cache = new ConcurrentDictionary<TIn, Lazy<TOut>>();
+            return x => cache.GetOrAdd(x, new Lazy<TOut>(() => function(x))).Value;
+        }
+
+        public static Func<T1, T2, TOut> Memoize<T1, T2, TOut>(this Func<T1, T2, TOut> function)
+        {
+            var cache = new ConcurrentDictionary<(T1, T2), Lazy<TOut>>();
+            return (x, y) => cache.GetOrAdd((x, y), new Lazy<TOut>(() => function(x, y))).Value;
         }
 
         public static Func<T, T> Identity<T>() => IdentityFunc<T>.Value;
---
+++ CS.Edu.Tests/MemoizationTests.cs
@@ -0,0 +1,60 @@
+﻿using CS.Edu.Core.Extensions;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests
+{
+    [TestFixture]
+    public class MemoizationTests
+    {
+        class TestClass
+        {
+            public int Counter { get; private set; }
+
+            public int Add(int one, int other)
+            {
+                Counter++;
+                return one + other;
+            }
+        }
+
+        [Test]
+        public void OneInvocation()
+        {
+            var test = new TestClass();
+            var memoized = Functions.Memoize<int, int, int>((x,y) => test.Add(x, y));
+            //var memoized = Functions.Memoize<(int, int), int>(x => test.Add(x.Item1, x.Item2));
+            //var memoized = Functions.Memoize<(int, int), int>(((int a, int b) x) => test.Add(x.a, x.b));
+            //var memoized = Functions.Memoize<int, Func<int, int>>(x => y => test.Add(x, y));
+            var result = memoized(2, 3);
+
+            Assert.AreEqual(5, result);
+            Assert.AreEqual(1, test.Counter);
+        }
+
+        [Test]
+        public void TwoInvocations_SameValue()
+        {
+            var test = new TestClass();
+            var memoized = Functions.Memoize<int, int, int>(test.Add);
+            var first = memoized(2, 3);
+            var second = memoized(2, 3);
+
+            Assert.AreEqual(5, first);
+            Assert.AreEqual(5, second);
+            Assert.AreEqual(1, test.Counter);
+        }
+
+        [Test]
+        public void TwoInvocations_DifferentValues()
+        {
+            var test = new TestClass();
+            var memoized = Functions.Memoize<int, int, int>(test.Add);
+            var first = memoized(3, 2); //тут вопрос как считать, параметры то одинаковые
+            var second = memoized(2, 3);
+
+            Assert.AreEqual(5, first);
+            Assert.AreEqual(5, second);
+            Assert.AreEqual(2, test.Counter);
+        }
+    }
+}
