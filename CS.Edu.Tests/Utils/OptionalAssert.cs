c12abffee2d3e09d21d379dd37c0a949b85f6300
bakanov-g <bakanov-g@speechpro.com>
2021-09-17 16:11:35 +0300

    OptionalAssert

---
+++ CS.Edu.Tests/Utils/OptionalAssert.cs
@@ -0,0 +1,24 @@
+using DynamicData.Kernel;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests.Utils
+{
+    public class OptionalAssert
+    {
+        public static void None<T>(Optional<T> optional)
+        {
+            Assert.IsFalse(optional.HasValue);
+        }
+
+        public static void Some<T>(Optional<T> optional)
+        {
+            Assert.IsTrue(optional.HasValue);
+        }
+
+        public static void Some<T>(Optional<T> optional, T expected)
+        {
+            Assert.IsTrue(optional.HasValue);
+            Assert.AreEqual(expected, optional.Value);
+        }
+    }
+}
