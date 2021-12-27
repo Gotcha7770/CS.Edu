2921fdd04cc85f5b92ac355b964a54e6e276ae20
Gotcha7770 <gbakanov@inbox.ru>
2021-12-27 14:48:13 +0300

    using top level statements

--- CS.Edu.Benchmarks/Program.cs
+++ CS.Edu.Benchmarks/Program.cs
@@ -1,42 +1,36 @@
 using System;
 using BenchmarkDotNet.Running;
+using CS.Edu.Benchmarks;
 using CS.Edu.Benchmarks.Extensions;
 
-namespace CS.Edu.Benchmarks
+var switcher = new BenchmarkSwitcher(new[]
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var switcher = new BenchmarkSwitcher(new[] {
-                typeof(FirstOrDefaultTypeBench),
-                typeof(RelationBench),
-                typeof(IsEvenBench),
-                typeof(IteratorBench),
-                typeof(PaginateBench),
-                typeof(BinarySplitBench),
-                typeof(TernarySplitBench),
-                typeof(ListBench),
-                typeof(FactorialBench),
-                typeof(FibonacciBench),
-                typeof(ToArrayVsToListBench),
-                typeof(AggregateManyBench),
-                typeof(PowerOfTwoBench),
-                typeof(RangeDiffBench),
-                typeof(InvalidateCollectionBench),
-                typeof(JaggedVsRectArrayBench),
-                typeof(DictionaryVsLookupBench),
-                typeof(DoubleEnumerationBench),
-                typeof(PullOutBench),
-                typeof(ExceptBench),
-                typeof(JoinBench),
-                typeof(AggregateBench),
-                typeof(EnumerableCreateBench),
-                typeof(MemberAccessBench)
-            });
+    typeof(FirstOrDefaultTypeBench),
+    typeof(RelationBench),
+    typeof(IsEvenBench),
+    typeof(IteratorBench),
+    typeof(PaginateBench),
+    typeof(BinarySplitBench),
+    typeof(TernarySplitBench),
+    typeof(ListBench),
+    typeof(FactorialBench),
+    typeof(FibonacciBench),
+    typeof(ToArrayVsToListBench),
+    typeof(AggregateManyBench),
+    typeof(PowerOfTwoBench),
+    typeof(RangeDiffBench),
+    typeof(InvalidateCollectionBench),
+    typeof(JaggedVsRectArrayBench),
+    typeof(DictionaryVsLookupBench),
+    typeof(DoubleEnumerationBench),
+    typeof(PullOutBench),
+    typeof(ExceptBench),
+    typeof(JoinBench),
+    typeof(AggregateBench),
+    typeof(EnumerableCreateBench),
+    typeof(MemberAccessBench),
+    typeof(EnumerableVsArrayCreation)
+});
 
-            switcher.Run();
-            Console.ReadLine();
-        }
-    }
-}
+switcher.Run();
+Console.ReadLine();
