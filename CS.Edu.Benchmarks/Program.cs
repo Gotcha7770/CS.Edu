57248cc0306f27755726568b173c07d71139383c
Gotcha7770 <gbakanov@inbox.ru>
2021-12-27 14:51:17 +0300

    using assembly loading

--- CS.Edu.Benchmarks/Program.cs
+++ CS.Edu.Benchmarks/Program.cs
@@ -1,36 +1,6 @@
 using System;
 using BenchmarkDotNet.Running;
-using CS.Edu.Benchmarks;
-using CS.Edu.Benchmarks.Extensions;
 
-var switcher = new BenchmarkSwitcher(new[]
-{
-    typeof(FirstOrDefaultTypeBench),
-    typeof(RelationBench),
-    typeof(IsEvenBench),
-    typeof(IteratorBench),
-    typeof(PaginateBench),
-    typeof(BinarySplitBench),
-    typeof(TernarySplitBench),
-    typeof(ListBench),
-    typeof(FactorialBench),
-    typeof(FibonacciBench),
-    typeof(ToArrayVsToListBench),
-    typeof(AggregateManyBench),
-    typeof(PowerOfTwoBench),
-    typeof(RangeDiffBench),
-    typeof(InvalidateCollectionBench),
-    typeof(JaggedVsRectArrayBench),
-    typeof(DictionaryVsLookupBench),
-    typeof(DoubleEnumerationBench),
-    typeof(PullOutBench),
-    typeof(ExceptBench),
-    typeof(JoinBench),
-    typeof(AggregateBench),
-    typeof(EnumerableCreateBench),
-    typeof(MemberAccessBench),
-    typeof(EnumerableVsArrayCreation)
-});
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
-switcher.Run();
 Console.ReadLine();
