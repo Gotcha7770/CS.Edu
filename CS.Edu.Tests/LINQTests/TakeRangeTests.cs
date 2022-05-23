21da1062a22693c92075051dd428acdb3b9799ff
Gotcha7770 <gbakanov@inbox.ru>
2022-05-23 22:31:32 +0300

    TakeRange -> range is from index to index

---
+++ CS.Edu.Tests/LINQTests/TakeRangeTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests.LINQTests;
+
+[TestFixture]
+public class TakeRangeTests
+{
+    [Test]
+    public void TakeRange()
+    {
+        var items = Enumerable.Range(0, 20);
+        var page1 = items.Skip(10).Take(20);
+        var page2 = items.Take(10..30);
+    }
+}
