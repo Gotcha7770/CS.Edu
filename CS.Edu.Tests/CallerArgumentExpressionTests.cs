2a05c81cd0df4236e1a7d4fc95abdab1fb5b71fd
Gotcha7770 <gbakanov@inbox.ru>
2022-07-30 17:18:16 +0300

    CallerArgumentExpression

---
+++ CS.Edu.Tests/CallerArgumentExpressionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using CS.Edu.Tests.Utils;
+using NUnit.Framework;
+
+namespace CS.Edu.Tests;
+
+[TestFixture]
+public class CallerArgumentExpressionTests
+{
+    public static void Check(bool condition, [CallerArgumentExpression("condition")] string message = default)
+    {
+        if (!condition)
+            throw new Exception($"Condition failed: {message}");
+    }
+
+    [Test]
+    public void METHOD()
+    {
+        //var item = new Identity<int>(3);
+        Identity<int> identity = null;
+        Assert.Throws<Exception>(() => Check(identity is { Key: 3 }));
+    }
+}
