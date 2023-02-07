43f09570bb984e3aa7f61f09a2d3ae6e9d24521a
Gotcha7770 <gbakanov@inbox.ru>
2023-02-07 23:41:34 +0300

    IAsyncEnumerable exercises

---
+++ CS.Edu.Core/Extensions/AsyncEnumerableExtensions/Traverse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS.Edu.Core.Extensions.AsyncEnumerableExtensions;
+
+public static partial class AsyncEnumerableExtensions
+{
+    // public static async IAsyncEnumerable<T> Sequence<T>(this IEnumerable<Task<T>> source)
+    // {
+    //     foreach (Task<T> task in source)
+    //     {
+    //         yield return await task;
+    //     }
+    // }
+
+    public static async Task<IEnumerable<T>> Sequence<T>(this IEnumerable<Task<T>> source)
+    {
+        var result = await Task.WhenAll(source);
+        return result;
+    }
+
+    public static async Task<IEnumerable<TResult>> Traverse<T, TResult>(
+        this IEnumerable<T> source,
+        Func<T, Task<TResult>> selector)
+    {
+        var result = await Task.WhenAll(source.Select(selector));
+        return result;
+    }
+}
--- CS.Edu.Tests/LINQTests/AsyncEnumerableTests.cs
+++ CS.Edu.Tests/LINQTests/AsyncEnumerableTests.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace CS.Edu.Tests.LINQTests;
 
+// https://www.dotnetcurry.com/csharp/async-streams
 public class AsyncEnumerableTests
 {
     private record Customer(Guid Id);
@@ -29,7 +29,7 @@ public class AsyncEnumerableTests
         return await Task.FromResult(orders);
     }
 
-    private IAsyncEnumerable<Order> GetOrdersEnumerable(Guid customerId)
+    private IAsyncEnumerable<Order> GetOrdersAsyncEnumerable(Guid customerId)
     {
         return Enumerable.Range(0, 5)
             .Select(_ => new Order(Guid.NewGuid(), customerId))
@@ -37,33 +37,59 @@ public class AsyncEnumerableTests
     }
 
     [Fact]
-    public async void ExampleWithTask()
+    public void UnwrapProblem()
+    {
+        // Here we are dealing with a combination of two effects
+        IEnumerable<Task<int>> items = null;
+
+        // Select operator treats the function result as a simple type.
+        // So you can transform Task element in Task with any other type,
+        // you can even transform it in IAsyncEnumerable (because it doesn't get rid of the async effect),
+        // But you can`t change the order of effects.
+        // Reversing the order of effects would require us to put an await between the variable and the evaluation,
+        // but IEnumerable doesn't know how to deal with await key word.
+        // Obviously it must be some other type
+        var enumeration = items.Select(async x => (await x).ToString());
+
+        // Hoverer, we have two options
+        // Transform IEnumerable<Task<T>> to Task<IEnumerable<T>>
+        var task = Task.WhenAll(items);
+
+        // or to IAsyncEnumerable<T>
+        var asyncEnumeration = items.ToAsyncEnumerable();
+    }
+
+    [Fact]
+    public async void SkipWithEnumerableOfTasks()
+    {
+        IEnumerable<Task<Order>> query = Enumerable.Range(0, 1)
+            .Select(_ => Task.FromResult(new Order(Guid.NewGuid(), _customers[1].Id)));
+
+        var singleItem = await query.Skip(5).First();
+
+    }
+
+    [Fact]
+    public async void PagingWithEnumerableOfTasks()
     {
         IEnumerable<Task<Order[]>> query = _customers.Select(x => GetOrdersTask(x.Id));
 
         foreach (var task in query)
         {
-            var order = await task;
-            // ...
+            var orders = await task;
+            // how to paging?
         }
     }
 
     [Fact]
-    public async Task ExampleWithAsyncEnumerable()
+    public async Task PagingWithAsyncEnumerable()
     {
         var query = _customers.ToAsyncEnumerable()
-            .SelectMany(x => GetOrdersEnumerable(x.Id));
+            .SelectMany(x => GetOrdersAsyncEnumerable(x.Id));
 
         await foreach (var order in query)
         {
-            //...
+            // we can get exactly as much as we want
         }
-    }
-
-    [Fact]
-    public async Task AsyncGroupBy()
-    {
-        var query = _customers.ToAsyncEnumerable()
-            .GroupBy(x => x.Id);
     }
 }
