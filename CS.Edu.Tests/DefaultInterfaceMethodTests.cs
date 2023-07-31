79f61a01fe84f493d675149402284f3cddff9912
Gotcha7770 <gbakanov.box@gmail.com>
2023-07-31 14:07:56 +0300

    Default Interface Method

---
+++ CS.Edu.Tests/DefaultInterfaceMethodTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CS.Edu.Tests;
+
+public class DefaultInterfaceMethodTests
+{
+    private static readonly DateTime Stamp = DateTime.Now;
+
+    interface IRemovable
+    {
+        bool IsRemoved { get; set; }
+        void Remove() => IsRemoved = true;
+    }
+
+    class Item : IRemovable
+    {
+        public bool IsRemoved { get; set; }
+    }
+
+    class ExtendedItem : IRemovable
+    {
+        public bool IsRemoved { get; set; }
+        public DateTime Removed { get; set; }
+
+        void IRemovable.Remove()
+        {
+            IsRemoved = true;
+            Removed = Stamp;
+        }
+    }
+
+    [Fact]
+    public void RemoveTest()
+    {
+        var items = new IRemovable[]
+        {
+            new Item(),
+            new ExtendedItem()
+        };
+
+        items.ForEach(x => x.Remove());
+
+        items[0]
+            .Should()
+            .BeOfType<Item>()
+            .Which.IsRemoved.Should()
+            .BeTrue();
+
+        items[1]
+            .Should()
+            .BeOfType<ExtendedItem>()
+            .Which.Should()
+            .BeEquivalentTo(new ExtendedItem
+            {
+                IsRemoved = true,
+                Removed = Stamp
+            });
+    }
+}
