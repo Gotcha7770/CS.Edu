f8a3600c495c961e88ff53f5be7d49263f49f338
Gotcha7770 <gbakanov.box@gmail.com>
2023-10-11 17:37:13 +0300

    AnyVsExistsBench

---
+++ CS.Edu.Benchmarks/AnyVsExistsBench.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace CS.Edu.Benchmarks;
+
+[MemoryDiagnoser]
+[Config(typeof(DefaultConfig))]
+public class AnyVsExistsBench
+{
+    private readonly List<int> _items = Enumerable.Range(0, 1_000_000)
+        .ToList();
+
+    [Benchmark]
+    public bool Any()
+    {
+        return _items.Any(x => x == 999999);
+    }
+
+    [Benchmark]
+    public bool Exists()
+    {
+        return _items.Exists(x => x == 999999);
+    }
+}
