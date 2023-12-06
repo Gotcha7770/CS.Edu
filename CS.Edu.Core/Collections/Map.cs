b45372a7bb1d7ec490f19fd3b39ed07e02c2463f
Gotcha7770 <gbakanov.box@gmail.com>
2023-12-06 20:12:44 +0300

    Add Map class to support bidirectional mapping.

---
+++ CS.Edu.Core/Collections/Map.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace CS.Edu.Core.Collections;
+
+public class Map<T1, T2>
+{
+    private readonly Dictionary<T1, T2> _forward = new Dictionary<T1, T2>();
+    private readonly Dictionary<T2, T1> _reverse = new Dictionary<T2, T1>();
+
+    public Map()
+    {
+        Forward = new Indexer<T1, T2>(_forward);
+        Reverse = new Indexer<T2, T1>(_reverse);
+    }
+
+    public void Add(T1 t1, T2 t2)
+    {
+        _forward.Add(t1, t2);
+        _reverse.Add(t2, t1);
+    }
+
+    public Indexer<T1, T2> Forward { get; }
+    public Indexer<T2, T1> Reverse { get; }
+}
+
+public class Indexer<T1, T2>
+{
+    private readonly Dictionary<T1, T2> _dictionary;
+
+    public Indexer(Dictionary<T1, T2> dictionary)
+    {
+        _dictionary = dictionary;
+    }
+
+    public T2 this[T1 index]
+    {
+        get { return _dictionary[index]; }
+    }
+}
