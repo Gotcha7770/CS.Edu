b3e1e7bd9b7d3af07ecf046789b1c6043768b9c6
Gotcha7770 <gbakanov.box@gmail.com>
2023-12-08 16:15:02 +0300

    bench get method with HttpCompletionOption

---
+++ CS.Edu.Benchmarks/HttpCompletionOptionBench.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using RichardSzalay.MockHttp;
+
+namespace CS.Edu.Benchmarks;
+
+[MemoryDiagnoser]
+[Config(typeof(DefaultConfig))]
+public class HttpCompletionOptionBench
+{
+    private const string PathToHTML = "CS.Edu.Benchmarks.Resources.document.html";
+    private const string Link = "http://localhost/api/documents/1";
+
+    private readonly MockHttpMessageHandler _mockHttp = new MockHttpMessageHandler();
+    private readonly HttpClient _client;
+
+    public HttpCompletionOptionBench()
+    {
+        string content;
+        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(PathToHTML))
+        using (var reader = new StreamReader(stream, Encoding.UTF8))
+        {
+            content = reader.ReadToEnd();
+        }
+
+        _mockHttp.When(Link)
+            .Respond("text/html", content);
+        _client = _mockHttp.ToHttpClient();
+    }
+
+    [Benchmark]
+    public Task<HttpResponseMessage> ResponseContentRead()
+    {
+        return _client.GetAsync(Link);
+    }
+
+    [Benchmark]
+    public Task<HttpResponseMessage> ResponseHeadersRead()
+    {
+        return _client.GetAsync(Link, HttpCompletionOption.ResponseHeadersRead);
+    }
+}
