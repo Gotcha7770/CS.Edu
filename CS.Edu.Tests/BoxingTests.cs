da8d04e660317659eaa36c4e05b4e3dd98da9cec
Gotcha7770 <gbakanov.box@gmail.com>
2024-01-17 15:22:49 +0300

    BoxingTests

---
+++ CS.Edu.Tests/BoxingTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace CS.Edu.Tests;
+
+public class BoxingTests
+{
+    [Fact]
+    public void Test()
+    {
+        int value = 6;
+        // object method call cause boxing
+        // IL_0004: box
+        var type = value.GetType();
+
+        // it`s like
+        var obj = (object)value;
+        type = obj.GetType();
+    }
+}
