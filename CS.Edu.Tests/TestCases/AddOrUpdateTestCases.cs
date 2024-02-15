343bd34a0ba47a78eb3443f29f9213dff5936e20
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-15 11:10:24 +0300

    fix

---
+++ CS.Edu.Tests/TestCases/AddOrUpdateTestCases.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using CS.Edu.Tests.Utils;
+using Xunit;
+
+namespace CS.Edu.Tests.TestCases;
+
+public class AddOrUpdateTestCases : TheoryData<IList<KeyValue>, KeyValue, IEnumerable<KeyValue>>
+{
+    public AddOrUpdateTestCases()
+    {
+        Add(new List<KeyValue>
+            {
+                new KeyValue(1, "One"),
+                new KeyValue(2, "Two")
+            },
+            new KeyValue(0, "Zero"),
+            [new KeyValue(0, "Zero"),  new KeyValue(1, "One"), new KeyValue(2, "Two")]);
+
+        Add(new List<KeyValue>
+            {
+                new KeyValue(1, "One"),
+                new KeyValue(2, "Two")
+            },
+            new KeyValue(1, "Changed"),
+            [ new KeyValue(1, "One/Changed"), new KeyValue(2, "Two")]);
+    }
+}
