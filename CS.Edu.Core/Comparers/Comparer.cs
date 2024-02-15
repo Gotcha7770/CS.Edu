78b51b1d1c56b9978ae4d61b052900f8b0f114f0
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-15 11:10:10 +0300

    DelegateComparer

---
+++ CS.Edu.Core/Comparers/Comparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS.Edu.Core.Comparers;
+
+public static class Comparer
+{
+    public static IComparer<T> Create<T>(Comparison<T> comparer)
+    {
+        return new DelegateComparer<T>(comparer);
+    }
+
+    private class DelegateComparer<T> : IComparer<T>
+    {
+        private readonly Comparison<T> _comparer;
+
+        public DelegateComparer(Comparison<T> comparer)
+        {
+            _comparer = comparer;
+        }
+
+        public int Compare(T x, T y) => _comparer(x, y);
+    }
+}
