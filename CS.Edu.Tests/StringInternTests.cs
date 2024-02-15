9f563469d9351b77ab1e2718ac9a160f3f4c8661
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-15 11:07:29 +0300

    String Intern

---
+++ CS.Edu.Tests/StringInternTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace CS.Edu.Tests;
+
+public class StringInternTests
+{
+    // Среда CLR поддерживает таблицу называемую пул интернирования.
+    // Эта таблица содержит одну уникальную ссылку на каждую строку,
+    // которая либо объявлена, либо создана программно во время выполнения вашей программы.
+
+    [Theory]
+    [MemberData(nameof(StringEqualityCases))]
+    public void StringEquality(string one, string other, bool expected)
+    {
+        one.Equals(other)
+            .Should()
+            .BeTrue();
+        ReferenceEquals(one, other)
+            .Should()
+            .Be(expected);
+    }
+
+    public static IEnumerable<object[]> StringEqualityCases
+    {
+        get
+        {
+            const string constant = "hello";
+            string variable = "hello";
+
+            yield return ["hello world", "hello" + " world", true];
+            yield return ["hello world", constant + " world", true];
+            yield return ["hello world", variable + " world", false];
+            yield return ["hello world", string.Intern(variable + " world"), true];
+        }
+    }
+}
