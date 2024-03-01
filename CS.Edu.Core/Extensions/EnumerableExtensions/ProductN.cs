638fa1fb67f14bf616b56c30f5031475d8b23cfd
Gotcha7770 <gbakanov.box@gmail.com>
2024-03-01 19:19:15 +0300

    Cartesian product of n sequences

---
+++ CS.Edu.Core/Extensions/EnumerableExtensions/ProductN.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace CS.Edu.Core.Extensions;
+
+public static partial class EnumerableExtensions
+{
+    // https://ericlippert.com/2010/06/28/computing-a-cartesian-product-with-linq/
+    // https://www.interact-sw.co.uk/iangblog/2010/07/28/linq-cartesian-1
+
+    public static IEnumerable<IEnumerable<T>> ProductN<T>(this IEnumerable<IEnumerable<T>> source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        return source.Aggregate(
+            EnumerableEx.Return(Enumerable.Empty<T>()),
+            (acc, cur) =>
+                from prevProductItem in acc
+                from item in cur
+                select prevProductItem.Append(item));
+    }
+
+    private static IEnumerable<IEnumerable<T>> Iterator<T>(IEnumerable<IEnumerable<T>> source)
+    {
+        using var enumerator = source.GetEnumerator();
+        if(!enumerator.MoveNext())
+            return Enumerable.Empty<IEnumerable<T>>();
+
+        var product = enumerator.Current.Select(EnumerableEx.Return);
+        while (enumerator.MoveNext())
+        {
+            var current = enumerator.Current;
+            product = from prevProductItem in product
+                      from item in current
+                      select prevProductItem.Append(item);
+        }
+
+        return product;
+    }
+}
---
+++ CS.Edu.Tests/Extensions/EnumerableExtensions/ProductNTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CS.Edu.Core.Extensions;
+using FluentAssertions;
+using Xunit;
+using static FluentAssertions.FluentActions;
+
+namespace CS.Edu.Tests.Extensions.EnumerableExtensions;
+
+public class ProductNTests
+{
+    [Fact]
+    public void SourceIsNull_ThrowsArgumentNullException()
+    {
+        IEnumerable<IEnumerable<int>> none = null;
+        Invoking(() => none.ProductN()).Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void SourceIsEmpty_ReturnsEmpty()
+    {
+        var items = Enumerable.Empty<IEnumerable<int>>();
+        items.ProductN()
+            .Should()
+            .BeEquivalentTo(EnumerableEx.Return(Enumerable.Empty<int>()));
+    }
+
+    [Fact]
+    public void TwoCollection_Returns4CollectionsOf2Elements()
+    {
+        int[][] collections =
+        [
+            [1, 2],
+            [3, 4]
+        ];
+
+        collections.ProductN()
+            .Should()
+            .BeEquivalentTo(new int[][]
+            {
+                [1, 3],
+                [1, 4],
+                [2, 3],
+                [2, 4]
+            });
+    }
+
+    [Fact]
+    public void ThreeCollections_Returns8CollectionsOf3Elements()
+    {
+        int[][] collections =
+        [
+            [1, 2],
+            [3, 4],
+            [5, 6]
+        ];
+
+        collections.ProductN()
+            .Should()
+            .BeEquivalentTo(new int[][] {
+                [1, 3, 5],
+                [1, 3, 6],
+                [1, 4, 5],
+                [1, 4, 6],
+                [2, 3, 5],
+                [2, 3, 6],
+                [2, 4, 5],
+                [2, 4, 6]
+            });
+    }
+}
