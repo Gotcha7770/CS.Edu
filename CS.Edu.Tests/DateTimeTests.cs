f81cd17386f5146d5717f215a17d57a8b21cf250
Gotcha7770 <gbakanov.box@gmail.com>
2024-03-15 14:51:54 +0300

    DateTime equality and comparison exercises

--- CS.Edu.Tests/DateTimeTests.cs
+++ CS.Edu.Tests/DateTimeTests.cs
@@ -34,4 +34,58 @@ public class DateTimeTests
             .Should()
             .Throw<ArgumentOutOfRangeException>();
     }
+
+    [Theory]
+    [ClassData(typeof(DateTimeKindEqualityCases))]
+    public void IsKindAffectedEquality(DateTime one, DateTime other, bool expected)
+    {
+        one.Equals(other)
+            .Should()
+            .Be(expected);
+    }
+
+    [Theory]
+    [ClassData(typeof(DateTimeKindComparisonCases))]
+    public void IsKindAffectedComparison(DateTime one, DateTime other, int expected)
+    {
+        one.CompareTo(other)
+            .Should()
+            .Be(expected);
+    }
+
+    public class DateTimeKindEqualityCases : TheoryData<DateTime, DateTime, bool>
+    {
+        public DateTimeKindEqualityCases()
+        {
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Unspecified),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local),
+                true);
+
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Unspecified),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Utc),
+                true);
+
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Utc),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local),
+                true);
+        }
+    }
+
+    public class DateTimeKindComparisonCases : TheoryData<DateTime, DateTime, int>
+    {
+        public DateTimeKindComparisonCases()
+        {
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Unspecified),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local),
+                0);
+
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Unspecified),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Utc),
+                0);
+
+            Add(new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Utc),
+                new DateTime(2012, 12, 12, 0, 0, 0, DateTimeKind.Local),
+                0);
+        }
+    }
 }
