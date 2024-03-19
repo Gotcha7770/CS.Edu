95b2ec4433d40c48182be3647757053485d775f6
Gotcha7770 <gbakanov.box@gmail.com>
2024-03-19 21:04:20 +0300

    custom Path exercises

---
+++ CS.Edu.Tests/IO/PathTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CS.Edu.Tests.TestCases;
+using FluentAssertions;
+using Xunit;
+
+namespace CS.Edu.Tests.IO;
+
+public class PathTests
+{
+    // https://ru.wikipedia.org/wiki/POSIX
+    // https://opensource.com/article/19/8/understanding-file-paths-linux
+    // https://pubs.opengroup.org/onlinepubs/009695399/basedefs/xbd_chap04.html#tag_04_11
+
+    public readonly record struct Path
+    {
+        private const char PosixSeparator = '/';
+        private readonly Segment[] _segments;
+
+        public static Path Empty { get; } = new Path(Array.Empty<Segment>(), false);
+
+        public Path(string value)
+            :this(value.Split(PosixSeparator, StringSplitOptions.RemoveEmptyEntries).Select(x => new Segment(x)), value.StartsWith('/'))
+        {
+        }
+
+        internal Path(IEnumerable<Segment> segments, bool isAbsolute)
+        {
+            _segments = segments.ToArray();
+            IsAbsolute = isAbsolute;
+        }
+
+        public bool IsAbsolute { get; }
+        public IReadOnlyCollection<Segment> Segments => _segments;
+
+        public static Path operator +(Path one, Path other)
+        {
+            return new Path([..one.Segments, ..other.Segments], one.IsAbsolute);
+        }
+
+        public static bool CanResolve(Path one, Path other) => !(one.IsAbsolute && other.IsAbsolute);
+
+        public static bool CanResolve(params Path[] values)
+        {
+            return values.Count(path => path.IsAbsolute) <= 1;
+        }
+
+        public static Path Resolve(Path one, Path other)
+        {
+            return CanResolve(one, other) ? Resolve(one) + Resolve(other) : Empty;
+        }
+
+        public static Path Resolve(params Path[] values)
+        {
+            return CanResolve(values) ? values.Aggregate(Empty, Resolve) : Empty;
+        }
+    }
+
+    public readonly record struct Segment
+    {
+        private readonly string _representation;
+
+        //TODO: portable filenames
+        internal Segment(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException(nameof(value));
+
+            _representation = value;
+            IsGoUp = value is "..";
+        }
+
+        public bool IsGoUp { get; }
+
+        public override string ToString()
+        {
+            return _representation;
+        }
+    }
+
+    [Theory]
+    [ClassData(typeof(PathParsingCases))]
+    public void PathParsing_Segments(string value, bool isAbsolute, Segment[] expected)
+    {
+        var path = new Path(value);
+
+        path.IsAbsolute.Should()
+            .Be(isAbsolute);
+        path.Segments.Should()
+            .BeEquivalentTo(expected);
+    }
+}
---
+++ CS.Edu.Tests/TestCases/PathParsingCases.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using Segment = CS.Edu.Tests.IO.PathTests.Segment;
+
+namespace CS.Edu.Tests.TestCases;
+
+public class PathParsingCases :TheoryData<string, bool, Segment[]>
+{
+    public PathParsingCases()
+    {
+        Add("", false, Array.Empty<Segment>());
+        Add("/", true, Array.Empty<Segment>());
+        Add("//", true, Array.Empty<Segment>());
+        Add("/home", true, [new Segment("home")]);
+        Add("//home", true, [new Segment("home")]);
+        Add("user", false, [new Segment("user")]);
+        Add("user/", false, [new Segment("user")]);
+        Add("user//", false, [new Segment("user")]);
+        Add("/home/user/documents", true, [new Segment("home"), new Segment("user"), new Segment("documents")]);
+        Add("user/documents", false, [new Segment("user"), new Segment("documents")]);
+        Add("/home/user/../cfg", true, [new Segment("home"), new Segment("user"), new Segment(".."), new Segment("cfg")]);
+        Add("user/../cfg/..", false, [new Segment("user"), new Segment(".."), new Segment("cfg"), new Segment("..")]);
+    }
+}
