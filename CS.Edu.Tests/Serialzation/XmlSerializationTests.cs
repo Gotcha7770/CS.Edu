23f4f4aa32199de25dda108d8053f231535c23b9
Gotcha7770 <gbakanov.box@gmail.com>
2024-05-12 15:14:31 +0300

    xml serialization

---
+++ CS.Edu.Tests/Serialzation/XmlSerializationTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace CS.Edu.Tests.Serialzation;
+
+public class LogEntry
+{
+    //[XmlAttribute(DataType = "dateTime")]
+    public DateTime DateTime
+    {
+        get;
+        init;
+    }
+
+    public string Message
+    {
+        get;
+        init;
+    }
+}
+
+public class XmlSerializationTests
+{
+    [Fact]
+    public void Serialize()
+    {
+        var serializer = new XmlSerializer(typeof(LogEntry));
+
+        var dateTime = DateTime.Now;
+        dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified);
+        var entry = new LogEntry
+        {
+            DateTime = dateTime,
+            Message = "Test message"
+        };
+        using TextWriter writer = new StringWriter();
+        serializer.Serialize(writer, entry);
+    }
+
+    [Fact]
+    public void DateTimeFormat()
+    {
+        //XmlMediaTypeFormatter
+        //XmlDateTimeSerializationMode.Unspecified
+
+        var serializer = new XmlSerializer(typeof(LogEntry));
+
+        var entry = new LogEntry
+        {
+            DateTime = DateTime.Now,
+            Message = "Test message"
+        };
+        using TextWriter writer = new StringWriter();
+        serializer.Serialize(writer, entry);
+    }
+}
