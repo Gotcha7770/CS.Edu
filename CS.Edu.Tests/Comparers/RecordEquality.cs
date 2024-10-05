b519d24168f2d0609f1f82c27e430bb7855c4784
Gotcha7770 <gbakanov.box@gmail.com>
2024-10-06 00:30:32 +0300

    reference type fields in records violate instance equivalence

---
+++ CS.Edu.Tests/Comparers/RecordEquality.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CS.Edu.Tests.Comparers;
+
+public class RecordEquality
+{
+    record Entity(int Id, string Tag, Entity[] Children);
+
+    [Fact]
+    public void EntityEquality()
+    {
+        var one = new Entity(1, "First", []);
+        var other = new Entity(1, "First", []);
+
+        one.Equals(other)
+            .Should()
+            .BeTrue();
+
+        one = one with { Children = [new Entity(2, "Second", [])] };
+        other = other with { Children = [new Entity(2, "Second", [])] };
+
+        one.Equals(other)
+            .Should()
+            .BeFalse();
+
+        one.Children.GetHashCode().Should().NotBe(other.GetHashCode());
+    }
+}
